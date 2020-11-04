9b86ffbd817c99708cc9bd4c7596ca42cc774ecc
Axel Fernandez <axelfernandez@eventbrite.com>
2020-11-03 21:56:13 -0300

    Config To Heroku

--- alarma_comunitaria_site/settings/production.py
+++ alarma_comunitaria_site/settings/production.py
@@ -17,8 +17,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
